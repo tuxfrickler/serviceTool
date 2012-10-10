a063f0fea76ca69abe9549713c92cd007d6a7681
Marco Endress <mendress@tuxfrickler.de>
2012-10-10 03:31:38 +0200

    first commit

---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceTool.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            /*
+            WindowManager wm = new WindowManager(App.Messenger);
+            MenuItemViewModel mivm = new MenuItemViewModel(App.Messenger);
+            this.DataContext = mivm;
+             */
+        }
+    }
+}
